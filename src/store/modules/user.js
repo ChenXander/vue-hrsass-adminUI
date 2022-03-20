64a79db736563b4eb0d557bdb3869c618e75cdb0
ChenXander <cx1197257655@gmail.com>
2022-03-20 19:27:43 +0800

    封装登录的action

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,5 +1,6 @@
 // 用户信息
 import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
 
 // 状态
 // 初始化的时候从缓存中读取状态 并赋值到初始化的状态上
@@ -21,7 +22,18 @@ const mutations = {
   }
 }
 // 执行异步
-const actions = {}
+const actions = {
+  // 定义login action  也需要参数 调用action时 传递过来的参数
+  async login(context, data) {
+    // 调用api接口
+    const result = await login(data)
+    // axios默认给数据加了一层data
+    if (result.data.success) {
+      // actions 修改state 必须通过mutations
+      context.commit('setToken', result.data.data)
+    }
+  }
+}
 
 export default {
   namespaced: true,
