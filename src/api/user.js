dc80db7733d07bd8bd23d82c498a6b21cee10e09
ChenXander <cx1197257655@gmail.com>
2022-03-20 15:53:50 +0800

    项目目录整理基础环境搭建

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,15 @@
-import request from '@/utils/request'
+// 用户api
+// import request from '@/utils/request'
 
+// 用户登录
 export function login(data) {
-  return request({
-    url: '/vue-admin-template/user/login',
-    method: 'post',
-    data
-  })
-}
 
+}
+// 用户token
 export function getInfo(token) {
-  return request({
-    url: '/vue-admin-template/user/info',
-    method: 'get',
-    params: { token }
-  })
-}
 
+}
+// 用户登出
 export function logout() {
-  return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
-  })
+
 }
