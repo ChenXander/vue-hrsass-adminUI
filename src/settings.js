c26cca41ea7b024a2758287028685986e1f52e5e
ChenXander <37923950+ChenXander@users.noreply.github.com>
2022-03-26 13:21:30 +0800

    Delete src directory

--- src/settings.js
+++
@@ -1,8 +0,0 @@
-// 项目信息配置
-module.exports = {
-  title: '人力资源管理平台', // 项目名称
-
-  fixedHeader: false, // 固定头部
-
-  sidebarLogo: false // 侧边栏logo
-}
--- src/store/getters.js
+++
@@ -1,7 +0,0 @@
-// vuex快捷访问
-const getters = {
-  sidebar: state => state.app.sidebar,
-  device: state => state.app.device,
-  token: state => state.user.token// 在根级的getters上开放子模块的属性
-}
-export default getters
