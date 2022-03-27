f1cf46e0484d549bb2b1bd130d99840964226470
ChenXander <cx1197257655@gmail.com>
2022-03-27 09:41:32 +0800

    首页轮播图组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,21 +1,37 @@
 <template>
   <div id="home">
+    <!-- 首页顶部导航 -->
     <nav-bar class="home-nav">
       <template v-slot:center>
         购物街
       </template>
     </nav-bar>
+
+    <home-swiper :banners="banners" />
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper.vue'
+
+import { getHomeMultidata } from 'network/home'
 
 export default {
   name: 'Home',
-  components: { NavBar },
+  components: { NavBar, HomeSwiper },
   data () {
-    return {}
+    return {
+      banners: [], // 轮播图数据
+      recommends: [] // 推荐
+    }
+  },
+  created () {
+    // 1.请求多个数据
+    getHomeMultidata().then(res => {
+      this.banners = res.banner.list
+      this.recommends = res.recommend
+    })
   },
   methods: {}
 }
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- 首页轮播图 -->
+    <swiper>
+        <swiper-item v-for="(item, index) in banners" :key="index">
+          <a :href="item.link">
+            <img :src="item.image" alt="">
+          </a>
+        </swiper-item>
+      </swiper>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from 'components/common/swiper'
+
+export default {
+  name: 'HomeSwiper',
+  components: { Swiper, SwiperItem },
+  props: {
+    banners: { // 轮播图
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
